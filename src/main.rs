f2cf6b5595fd51c5ce00a67e1adee04c8d5eb4c2
Jaku-BB <jaku.bb@outlook.com>
2023-09-22 23:10:04 +0200

    fix: server doesn't exit immediately now

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 mod server;
 
 use crate::server::initialize_server;
-use std::thread;
 
 fn main() {
-    thread::spawn(|| {
-        initialize_server("127.0.0.1:2137");
-    });
+    initialize_server("127.0.0.1:2137");
 }
